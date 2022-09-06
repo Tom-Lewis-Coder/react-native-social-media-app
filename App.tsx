38fd8905b05c40c0f1f33e61573c4f833fd41df6
Tom-Lewis-Coder <tomlewis139@gmail.com>
2022-09-06 16:15:38 +0100

    added AWS amplify for backend

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Amplify } from 'aws-amplify'
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import awsconfig from './src/aws-exports'
+
+Amplify.configure(awsconfig)
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "reactnativesocialmed7f7e59db": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
